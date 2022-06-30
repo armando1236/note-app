fc486c773370142673eca61050cffd8fa9d84f28
amjr86-DBxjRd <amjr86@outlook.com>
2022-06-29 17:57:18 -0700

    work on index.js

--- Develop/server.js
+++ Develop/server.js
@@ -16,46 +16,51 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // static middleware
-app.use(express.static('public'));
+app.use(express.static('./public'));
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
 
-// GET Route for feedback page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
-
-// POST Route for a new UX/UI tip
-app.post("/notes", (req, res) => {
-  const newNotes = req.body;
-  const notes = JSON.parsel(fs.readFileSync("./db/db.json"));
-  // fs.readFileAsync("./develop/db/db.json", "utf8").then(function(data){
-  // note.id = notes.length +1
-  newNotes.id = uuid.v4();
-  notes.push(newNotes);
-  // return notes
-}).then(function(notes){
-  fs.writeFileSync("./db/db.json", JSON.stringify(notes))
-  res.json(noteList);
-})
-  // const { title, text } = req.body;
-
-  if (req.body) {
-    const newNote = {
-      title,
-      text,
-    };
-
-    readAndAppend(newTip, './db/db.json');
-    res.json(`Note added! 🚀`);
-  } else {
-    res.error('Error in adding tip');
-  }
+app.get("api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, '/db/db.json'))
 });
+
+// POST Route 
+app.post("api/notes", (req, res) => {
+  const newNotes = req.body;
+  const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+  // fs.readFileAsync("./develop/db/db.json", "utf8").then(function(data){
+    // note.id = notes.length +1
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    // return notes
+    // }).then(function(notes){
+      fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+      res.json(notes);
+    });
+    // const { title, text } = req.body;
+    
+    // if (req.body) {
+      //   const newNote = {
+        //     title,
+        //     text,
+        //   };
+        
+        //   readAndAppend(newTip, './db/db.json');
+        //   res.json(`Note added! 🚀`);
+        // } else {
+          //   res.error('Error in adding tip');
+          // }
+          // });
+          
+          // GET Route for homepage
+          app.get('/', (req, res) =>
+          res.sendFile(path.join(__dirname, '/public/index.html'))
+          );
+          
+          // GET Route for feedback page
+          app.get('/notes', (req, res) =>
+          res.sendFile(path.join(__dirname, '/public/notes.html'))
+          );
+      
+          app.listen(PORT, () =>
+          console.log(`App listening at http://localhost:${PORT} 🚀`)
+          );
